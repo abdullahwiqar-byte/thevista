2ce253fb5a10ad86f05e5f69a00494237c0db684
abdullahwiqar-byte <abdullah.wiqar@gmail.com>
2025-10-12 18:04:17 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import Gallery from "./Gallery.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/gallery" element={<Gallery />} />
+    </Routes>
+  </BrowserRouter>
 );
